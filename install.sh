6625d370b2440677345502692c5b7fae2fe2b73b
Dylan Tackoor <mynameisdylantackoor@gmail.com>
2022-05-18 20:18:03 -0400

    moar updated

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@
 
 rsync -r ~/dylantackoor/deck-controls/configs/dolphin/ ~/.steam/steam/controller_base/templates/
 rsync -r ~/dylantackoor/deck-controls/configs/yuzu/ ~/.steam/steam/controller_base/templates/
+rsync -r ~/dylantackoor/deck-controls/configs/n64/ ~/.steam/steam/controller_base/templates/
